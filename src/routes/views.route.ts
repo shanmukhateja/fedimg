9c1e9b7f30315070db6fae79923efc14b3d7786b
Surya Teja K <shanmukhateja@gmail.com>
2023-12-10 18:18:57 +0530

    feat: Add UI to navigate to /home from root

--- src/routes/views.route.ts
+++ src/routes/views.route.ts
@@ -3,5 +3,7 @@ import { Router } from "express";
 export const viewsRouter = Router();
 
 viewsRouter.get('', (req, res) => {
-    res.render('landing.njk');
+    res.render('landing.njk', {
+        isAuthenticated: req.isAuthenticated()
+    });
 })
