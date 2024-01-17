37a1eacee208fe3287e23a526bd9dbe0a338be9d
Surya Teja K <shanmukhateja@gmail.com>
2024-01-17 22:19:26 +0530

    feat: QoL improvements
    - rename unit test db file
    
    - fix test:ci script failing with auth error
    
    - add "clean" npm script

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,11 +6,11 @@ import { isTesting } from "./utils/misc.js";
 
 export const AppDataSource = new DataSource({
     type: isTesting() ? "better-sqlite3" : "mysql",
-    host: process.env.FEDIMG_DB_HOSTNAME || "localhost",
+    host: process.env.FEDIMG_DB_HOSTNAME,
     port: parseInt(process.env.FEDIMG_DB_PORT) || 3306,
-    username: process.env.FEDIMG_DB_USER || "test",
-    password: process.env.FEDIMG_DB_PWD || "",
-    database: process.env.FEDIMG_DB_NAME || "test",
+    username: process.env.FEDIMG_DB_USER,
+    password: process.env.FEDIMG_DB_PWD,
+    database: isTesting() ? './test.sqlite3' : process.env.FEDIMG_DB_NAME,
     synchronize: true,
     logging: isTesting() ? false: 'all',
     entities: [User, Image],
