shanmukhateja/fedimg

commit 21861186e308828e66d129668aaf06148c245669

fix: use body-parser.raw() for /inbox URLs
This fixes a bug with whitespace difference causing Digest checksum mismatch

src/index.ts
@@ -37,11 +37,19 @@ import { getAssetsPath, getContentUploadPath } from './utils/path.js';
     app.use(sessionConfig);
     app.use(passport.session());
 
-    app.use([
-        express.json(),
-        express.urlencoded({ extended: true }),
-        cors()
-    ]);
+    // `req.body` for /inbox URLs cannot be parsed as JSON 
+    // as it can lead to Digest calculation mismatch. 
+    const jsonParser = express.json();
+    const rawParser = express.raw({ type: "application/json" });
+
+    app.use((req, res, next) => {
+        if (req.path.includes('/inbox')) {
+            return rawParser(req, res, next);
+        }
+        jsonParser(req, res, next);
+    });
+
+    app.use(cors());
 
     // Nunjucks
     nunjucks.configure('src/views', {

src/middlewares/signature.middleware.ts
@@ -37,5 +37,11 @@ export async function ensureSigned(req: Request, res: Response, next: NextFuncti
     }
 
 
+    // Transform body to JSON for simplicity.
+    try {
+        req.body = JSON.parse(req.body);
+    } catch (ignore) {
+        
+    }
     next();
 }

commit da2ab362d1331b5daa8f82063b2c45c633c29a28

fix for regression with 21861186e308828e66d129668aaf06148c245669

src/index.ts
@@ -47,10 +47,13 @@ import { nodeInfoRouter } from './routes/nodeinfo.route.js';
         if (req.path.includes('/inbox')) {
             return rawParser(req, res, next);
         }
-        jsonParser(req, res, next);
+        return jsonParser(req, res, next);
     });
 
-    app.use(cors());
+    app.use([
+        cors(),
+        express.urlencoded({extended: true})
+    ]);
 
     // Nunjucks
     nunjucks.configure('src/views', {